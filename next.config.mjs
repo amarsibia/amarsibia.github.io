9e283479f5e0cd06e1d8a4712563111b9ef40a29
amar <amar@weareopenr.com>
2025-02-21 17:07:29 +0000

    fix: web images not loading

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,7 @@ const nextConfig = {
   },
   images: {
     // Enable image optimization
-    unoptimized: false,
+    unoptimized: true,
     domains: [], // Add domains for external images if needed
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
